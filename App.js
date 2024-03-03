d1ad2a78cf3e021c091bf0d46665fb0149445e54
suttha jongjaroen <6410615121@cn330>
2024-03-03 11:11:26 +0700

    update nav

--- App.js
+++ App.js
@@ -1,20 +1,37 @@
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { NavigationContainer } from "@react-navigation/native";
+import React from 'react';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
 
-import HomeScreen from './components/HomeScreen'
-import Courses from "./components/Courses";
-import Forums from "./components/Forums";
+import HomeScreen from './components/HomeScreen';
+import ProfileScreen from './components/ProfileScreen';
+import NotificationsScreen from './components/NotificationsScreen';
+import SettingsScreen from './components/SettingsScreen';
+import CoursesScreen from './components/CoursesScreen';
+import ForumsScreen from './components/ForumsScreen';
 
-const Tab = createBottomTabNavigator();
 
-export default function App(){
-  return(
+
+const Stack = createStackNavigator();
+const BottomTab = createBottomTabNavigator();
+
+const BottomTabNavigator = () => (
+  <BottomTab.Navigator screenOptions={{headerShown: false}}>
+    <BottomTab.Screen name="Home" component={HomeScreen} />
+    <BottomTab.Screen name="Courses" component={CoursesScreen} />
+    <BottomTab.Screen name="Forums" component={ForumsScreen} />
+  </BottomTab.Navigator>
+);
+
+export default function App() {
+  return (
     <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name='Home' component={HomeScreen} />
-        <Tab.Screen name='Courses' component={Courses} />
-        <Tab.Screen name='Forums' component={Forums} />
-      </Tab.Navigator>
+      <Stack.Navigator>
+        <Stack.Screen name="Main" component={BottomTabNavigator} />
+        <Stack.Screen name="ProfileStack" component={ProfileScreen} />
+        <Stack.Screen name="NotificationsStack" component={NotificationsScreen} />
+        <Stack.Screen name="SettingsStack" component={SettingsScreen} />
+      </Stack.Navigator>
     </NavigationContainer>
-  )
+  );
 }
---
+++ components/NotificationsScreen.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function NotificationsScreen() {
+  return (
+    <View>
+      <Text>NotificationsScreen</Text>
+    </View>
+  )
+}
