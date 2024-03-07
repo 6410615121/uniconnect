74a1f178b968fa84e6a1ee044ca39cbdaa0d036a
suttha jongjaroen <6410615121@cn330>
2024-03-07 22:17:33 +0700

    update notificationScreen

--- components/Screens/NotificationsScreen.js
+++ components/Screens/NotificationsScreen.js
@@ -1,10 +1,24 @@
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity} from 'react-native'
 import React from 'react'
+
+function NotificationList(){
+  const list = new Array(20).fill('Notification Text');
+
+  return(
+    <View>
+      {list.map((item, index) => (
+        <TouchableOpacity key={index} onPress={() => alert(`Pressed notification ${index}`)}>
+          <Text>{item} {index}</Text>
+        </TouchableOpacity>
+      ))}
+    </View>
+  );
+}
 
 export default function NotificationsScreen() {
   return (
     <View>
-      <Text>NotificationsScreen</Text>
+      <NotificationList />
     </View>  
   )
 }
