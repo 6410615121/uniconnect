3c63277530b672ddabaf5ccdba89f3837ea3b0bd
suttha jongjaroen <6410615121@cn330>
2024-03-11 22:35:29 +0700

    added Simple Login and Logout button

--- App.js
+++ App.js
@@ -1,94 +1,167 @@
-import 'react-native-gesture-handler'
-import React from 'react';
-import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { createStackNavigator } from '@react-navigation/stack';
-import {icons} from './style/icon.js'
-import { NavigationContainer} from '@react-navigation/native';
-import { Button, Text, View,TouchableOpacity,Image} from 'react-native';
-import { TransitionPresets } from '@react-navigation/stack'; //https://reactnavigation.org/docs/stack-navigator/#transparent-modals
+import "react-native-gesture-handler";
+import React, { useState } from "react";
+import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createStackNavigator } from "@react-navigation/stack";
+import { icons } from "./style/icon.js";
+import { NavigationContainer } from "@react-navigation/native";
+import { Button, Text, View, TouchableOpacity, Image } from "react-native";
+import { TransitionPresets } from "@react-navigation/stack"; //https://reactnavigation.org/docs/stack-navigator/#transparent-modals
+
 import {
-  CoursesScreen, 
-  ForumsScreen, 
-  HomeScreen, 
-  NotificationsScreen, 
-  ProfileScreen, 
+  CoursesScreen,
+  ForumsScreen,
+  HomeScreen,
+  NotificationsScreen,
+  ProfileScreen,
   SettingsScreen,
   CourseDetailScreen,
   FileDetailScreen,
-  PostDetailScreen
-} from './components/Screens/Screens.js'
+  PostDetailScreen,
+  LoginScreen,
+} from "./components/Screens/Screens.js";
 
 const Stack = createStackNavigator();
 const BottomTab = createBottomTabNavigator();
 const TopTab = createMaterialTopTabNavigator();
 const BottomTabNavigator = () => (
-  <BottomTab.Navigator screenOptions={{headerShown: false}}>
-    <BottomTab.Screen name="Home" component={HomeScreen} options={{tabBarIcon: () => (
-            <Image source={require('./icon/home-icon.png')}/>
-          ),}}/>
-    <BottomTab.Screen name="Courses" component={CoursesScreen} options={{tabBarIcon: () => (
-            <Image source={require('./icon/course-icon.png')}/>
-          ),}}/>
-    <BottomTab.Screen name="Forums" component={ForumsScreen} options={{tabBarIcon: () => (
-            <Image source={require('./icon/forum-icon.png')}/>
-          ),}}/>
+  <BottomTab.Navigator screenOptions={{ headerShown: false }}>
+    <BottomTab.Screen
+      name="Home"
+      component={HomeScreen}
+      options={{
+        tabBarIcon: () => <Image source={require("./icon/home-icon.png")} />,
+      }}
+    />
+    <BottomTab.Screen
+      name="Courses"
+      component={CoursesScreen}
+      options={{
+        tabBarIcon: () => <Image source={require("./icon/course-icon.png")} />,
+      }}
+    />
+    <BottomTab.Screen
+      name="Forums"
+      component={ForumsScreen}
+      options={{
+        tabBarIcon: () => <Image source={require("./icon/forum-icon.png")} />,
+      }}
+    />
   </BottomTab.Navigator>
 );
 
 const TopTabcourse = ({ route }) => {
   const { course } = route.params;
-  return(
-  <TopTab.Navigator screenOptions={{headerShown: false}} >
-    <TopTab.Screen name="reviews" component={CourseDetailScreen} initialParams={{ course }} />
-    <TopTab.Screen name="sheets" component={CourseDetailScreen} initialParams={{ course }} />
-    <TopTab.Screen name="exam" component={CourseDetailScreen} initialParams={{ course }} />
-  </TopTab.Navigator>
+  return (
+    <TopTab.Navigator screenOptions={{ headerShown: false }}>
+      <TopTab.Screen
+        name="reviews"
+        component={CourseDetailScreen}
+        initialParams={{ course }}
+      />
+      <TopTab.Screen
+        name="sheets"
+        component={CourseDetailScreen}
+        initialParams={{ course }}
+      />
+      <TopTab.Screen
+        name="exam"
+        component={CourseDetailScreen}
+        initialParams={{ course }}
+      />
+    </TopTab.Navigator>
   );
-  };
-
-
+};
 
 export default function App() {
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  const handleLogin = () => {
+    setLoggedIn(true);
+  };
+
+  const handleLogout = () => {
+    setLoggedIn(false);
+    console.log('set logged out')
+  };
+
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Main" 
-        component={BottomTabNavigator} 
-        options={({ navigation }) => ({
-          headerTitle: () => <View></View>,
-          headerLeft: () => <TouchableOpacity  onPress={() => navigation.navigate('ProfileStack')}>
-            <Image
-                  source={require('./icon/default-profile-icon.png')} // path to image
-                  style={icons.profile_icon}
-            />  
-          </TouchableOpacity>,
-          headerRight: () => 
-            <View style={{flexDirection:'row'}}>
-              <TouchableOpacity  onPress={() => navigation.navigate('NotificationsStack')}>
-                <Image
-                  source={require('./icon/bell-icon.png')} // path to image
-                  style={icons.bell_icon}
-                />  
-              </TouchableOpacity>
-              <TouchableOpacity  onPress={() => navigation.navigate('SettingsStack')}>
-                <Image
-                  source={require('./icon/setting-icon.png')} // path to image
-                  style={icons.setting_icon}
-                />  
-              </TouchableOpacity>
-            </View>
-      })}/>
-        <Stack.Screen name="ProfileStack" component={ProfileScreen}/>
-        <Stack.Screen name="NotificationsStack" component={NotificationsScreen} />
-        <Stack.Screen name="SettingsStack"  component={SettingsScreen} />
-        <Stack.Screen name="CourseDetail" component={TopTabcourse} options={({ navigation }) => ({
-          headerTitle: () => <View></View>})}/>
-        <Stack.Screen name="FileDetail" options={{ ...TransitionPresets.ModalPresentationIOS,}} component={FileDetailScreen}/>
-        <Stack.Screen name="PostDetail" options={{ ...TransitionPresets.Modal,}} component={PostDetailScreen}/>
-      </Stack.Navigator>
+      {!loggedIn ? (
+        <Stack.Navigator>
+          <Stack.Screen name="Login">
+            {(props) => <LoginScreen {...props} setLoggedIn={handleLogin} />}
+          </Stack.Screen>
+        </Stack.Navigator>
+      ) : (
+        <Stack.Navigator>
+          <Stack.Screen
+            name="Main"
+            component={BottomTabNavigator}
+            options={({ navigation }) => ({
+              headerTitle: () => <View></View>,
+              headerLeft: () => (
+                <TouchableOpacity
+                  onPress={() => navigation.navigate("ProfileStack")}
+                >
+                  <Image
+                    source={require("./icon/default-profile-icon.png")} // path to image
+                    style={icons.profile_icon}
+                  />
+                </TouchableOpacity>
+              ),
+              headerRight: () => (
+                <View style={{ flexDirection: "row" }}>
+                  <TouchableOpacity
+                    onPress={() => navigation.navigate("NotificationsStack")}
+                  >
+                    <Image
+                      source={require("./icon/bell-icon.png")} // path to image
+                      style={icons.bell_icon}
+                    />
+                  </TouchableOpacity>
+                  <TouchableOpacity
+                    onPress={() => navigation.navigate("SettingsStack")}
+                  >
+                    <Image
+                      source={require("./icon/setting-icon.png")} // path to image
+                      style={icons.setting_icon}
+                    />
+                  </TouchableOpacity>
+                </View>
+              ),
+            })}
+          />
+          <Stack.Screen name="ProfileStack" component={ProfileScreen} />
+          <Stack.Screen
+            name="NotificationsStack"
+            component={NotificationsScreen}
+          />
+          <Stack.Screen name="SettingsStack">
+            {(props) => <SettingsScreen {...props} setLoggedOut= {handleLogout}/>}
+          </Stack.Screen>
+
+          <Stack.Screen
+            name="CourseDetail"
+            component={TopTabcourse}
+            options={({ navigation }) => ({
+              headerTitle: () => <View></View>,
+            })}
+          />
+          <Stack.Screen
+            name="FileDetail"
+            options={{ ...TransitionPresets.ModalPresentationIOS }}
+            component={FileDetailScreen}
+          />
+          <Stack.Screen
+            name="PostDetail"
+            options={{ ...TransitionPresets.Modal }}
+            component={PostDetailScreen}
+          />
+        </Stack.Navigator>
+      )}
     </NavigationContainer>
   );
 }
 
-export {BottomTabNavigator}
+export { BottomTabNavigator };
--- components/Screens/LoginScreen.js
+++ components/Screens/LoginScreen.js
@@ -1,12 +1,23 @@
-import { Text, View } from 'react-native'
+import { Button, Text, View } from 'react-native'
 import React, { Component } from 'react'
+import { TextInput, TouchableOpacity } from 'react-native-gesture-handler'
 
-export default class LoginScreen extends Component {
-  render() {
+export default function LoginScreen({setLoggedIn}){
     return (
-      <View>
-        <Text>LoginScreen</Text>
+      <View style={{justifyContent: 'center', alignItems: 'center', height: '100%'}}>
+        <Text>Login Screen</Text>
+        <TextInput 
+            style={{backgroundColor: '#C7C7C7', padding:5}}
+            placeholder="Enter Username Here"
+        />
+        <TextInput 
+            style={{backgroundColor: '#C7C7C7', padding:5}}
+            placeholder="Enter Password Here"
+        />
+        <Button 
+            title='Login'
+            onPress={setLoggedIn}
+        />
       </View>
     )
   }
-}
