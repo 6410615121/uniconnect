cb74b0c239ea36608cf8f1c6b4d37e70353af4cf
suttha jongjaroen <6410615121@cn330>
2024-04-08 13:05:13 +0700

    update App.js

--- App.js
+++ App.js
@@ -40,94 +40,95 @@ export default function App() {
   return (
     <NavigationContainer>
       {!loggedIn ? (
-        <RegisterAndLoginScreen />
+        <RegisterAndLoginStack handleLogin={handleLogin} />
       ) : (
-        <Stack.Navigator>
-          <Stack.Screen
-            name="Main"
-            component={BottomTabNavigator}
-            options={({ navigation }) => ({
-              headerTitle: () => <View></View>,
-              headerLeft: () => (
-                <TouchableOpacity
-                  onPress={() => navigation.navigate("ProfileStack")}
-                >
-                  <Image
-                    source={require("./icon/default-profile-icon.png")} // path to image
-                    style={icons.profile_icon}
-                  />
-                </TouchableOpacity>
-              ),
-              headerRight: () => (
-                <View style={{ flexDirection: "row" }}>
-                  <TouchableOpacity
-                    onPress={() => navigation.navigate("NotificationsStack")}
-                  >
-                    <Image
-                      source={require("./icon/bell-icon.png")} // path to image
-                      style={icons.bell_icon}
-                    />
-                  </TouchableOpacity>
-                  <TouchableOpacity
-                    onPress={() => navigation.navigate("SettingsStack")}
-                  >
-                    <Image
-                      source={require("./icon/setting-icon.png")} // path to image
-                      style={icons.setting_icon}
-                    />
-                  </TouchableOpacity>
-                </View>
-              ),
-            })}
-          />
-
-          <Stack.Screen name="ProfileStack" component={ProfileScreen} />
-
-          <Stack.Screen
-            name="NotificationsStack"
-            component={NotificationsScreen}
-          />
-
-          <Stack.Screen name="SettingsStack">
-            {(props) => (
-              <SettingsScreen {...props} setLoggedOut={handleLogout} />
-            )}
-          </Stack.Screen>
-
-          <Stack.Screen
-            name="CourseDetail"
-            component={TopTabcourse}
-            options={({ navigation }) => ({
-              headerTitle: () => <View></View>,
-            })}
-          />
-
-          <Stack.Screen
-            name="FileDetail"
-            options={{ ...TransitionPresets.ModalPresentationIOS }}
-            component={FileDetailScreen}
-          />
-
-          <Stack.Screen
-            name="PostDetail"
-            options={{ ...TransitionPresets.Modal }}
-            component={PostDetailScreen}
-          />
-        </Stack.Navigator>
+        <MainApp handleLogout={handleLogout} />
       )}
     </NavigationContainer>
   );
 }
 
-const RegisterAndLoginScreen= () => {
-  <Stack.Navigator>
-    <Stack.Screen name="Login">
-      {(props) => <LoginScreen {...props} setLoggedIn={handleLogin} />}
-    </Stack.Screen>
-    <Stack.Screen name="register">
-      {(props) => <RegisterScreen {...props} />}
-    </Stack.Screen>
-  </Stack.Navigator>  
-}
+const RegisterAndLoginStack = ({ handleLogin }) => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Login">
+        {(props) => <LoginScreen {...props} setLoggedIn={handleLogin} />}
+      </Stack.Screen>
+      <Stack.Screen name="register">
+        {(props) => <RegisterScreen {...props} />}
+      </Stack.Screen>
+    </Stack.Navigator>
+  );
+};
 
+const MainApp = ({ handleLogout }) => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="Main"
+        component={BottomTabNavigator}
+        options={({ navigation }) => ({
+          headerTitle: () => <View></View>,
+          headerLeft: () => (
+            <TouchableOpacity
+              onPress={() => navigation.navigate("ProfileStack")}
+            >
+              <Image
+                source={require("./icon/default-profile-icon.png")} // path to image
+                style={icons.profile_icon}
+              />
+            </TouchableOpacity>
+          ),
+          headerRight: () => (
+            <View style={{ flexDirection: "row" }}>
+              <TouchableOpacity
+                onPress={() => navigation.navigate("NotificationsStack")}
+              >
+                <Image
+                  source={require("./icon/bell-icon.png")} // path to image
+                  style={icons.bell_icon}
+                />
+              </TouchableOpacity>
+              <TouchableOpacity
+                onPress={() => navigation.navigate("SettingsStack")}
+              >
+                <Image
+                  source={require("./icon/setting-icon.png")} // path to image
+                  style={icons.setting_icon}
+                />
+              </TouchableOpacity>
+            </View>
+          ),
+        })}
+      />
 
+      <Stack.Screen name="ProfileStack" component={ProfileScreen} />
+
+      <Stack.Screen name="NotificationsStack" component={NotificationsScreen} />
+
+      <Stack.Screen name="SettingsStack">
+        {(props) => <SettingsScreen {...props} setLoggedOut={handleLogout} />}
+      </Stack.Screen>
+
+      <Stack.Screen
+        name="CourseDetail"
+        component={TopTabcourse}
+        options={({ navigation }) => ({
+          headerTitle: () => <View></View>,
+        })}
+      />
+
+      <Stack.Screen
+        name="FileDetail"
+        options={{ ...TransitionPresets.ModalPresentationIOS }}
+        component={FileDetailScreen}
+      />
+
+      <Stack.Screen
+        name="PostDetail"
+        options={{ ...TransitionPresets.Modal }}
+        component={PostDetailScreen}
+      />
+    </Stack.Navigator>
+  );
+};
