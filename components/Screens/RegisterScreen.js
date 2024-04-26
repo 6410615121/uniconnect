c5fd32bd1e1f7e6eac4fe43487137c81b8ead7e5
phum <6410455015@student.tu.ac.th>
2024-04-27 01:58:53 +0700

    update register screen

--- components/Screens/RegisterScreen.js
+++ components/Screens/RegisterScreen.js
@@ -1,4 +1,4 @@
-import { View, Text, TextInput, Button } from "react-native";
+import { View, Text, TextInput, Button, Image } from "react-native";
 import React, { useState } from "react";
 // import axios from "axios";
 // import { API_host, API_port } from "@env";
@@ -39,34 +39,34 @@ export default function RegisterScreen({ navigation }) {
 
   return (
     <View
-      style={{ justifyContent: "center", alignItems: "center", height: "100%" }}
+      style={{ justifyContent: "center", alignItems: "center", height: "100%" , backgroundColor:'#0C2D57'}}
     >
-      <Text>Register Screen</Text>
+      <Image source={require("../../assets/icons/logo.png")}/>
       <TextInput
-        style={{ backgroundColor: "#C7C7C7", padding: 5, width: 200 }}
+        style={{ backgroundColor: "#FFF8E3", paddingLeft:10, padding: 5, width: 200, borderWidth:1,borderRadius:15, marginBottom:10}}
         placeholder="Enter Email Here"
         onChangeText={(text) => setEmail(text)}
       />
       <TextInput
-        style={{ backgroundColor: "#C7C7C7", padding: 5, width: 200 }}
+        style={{ backgroundColor: "#FFF8E3", paddingLeft:10, padding: 5, width: 200, borderWidth:1,borderRadius:15, marginBottom:10}}
         placeholder="Enter your name Here"
         onChangeText={(text) => setName(text)}
       />
       <TextInput
-        style={{ backgroundColor: "#C7C7C7", padding: 5, width: 200 }}
+        style={{ backgroundColor: "#FFF8E3", paddingLeft:10, padding: 5, width: 200, borderWidth:1,borderRadius:15, marginBottom:10}}
         placeholder="Enter Password Here"
         onChangeText={(text) => setPassword(text)}
         secureTextEntry={true}
       />
 
       <TextInput
-        style={{ backgroundColor: "#C7C7C7", padding: 5, width: 200 }}
+        style={{ backgroundColor: "#FFF8E3", paddingLeft:10, padding: 5, width: 200, borderWidth:1,borderRadius:15, marginBottom:10}}
         placeholder="Password Confirm"
         onChangeText={(text) => setConfirmPassword(text)}
         secureTextEntry={true}
       />
       {registerMessage ? <Text>{registerMessage}</Text> : null}
-      <Button title="submit" onPress={handleRegister} />
+      <Button title="submit" onPress={handleRegister} color={'#FC6736'}/>
     </View>
   );
 }
